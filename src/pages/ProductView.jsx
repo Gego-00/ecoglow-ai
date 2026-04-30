f3160037ddc8de75f2320fd0f4430c1d80cd867b
Gego-00 <ggzambrna@gmail.com>
2026-04-30 09:27:59 -0600

    Imports fix

--- src/pages/ProductView.jsx
+++ src/pages/ProductView.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, <Link:import></Link:import> } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import { 
   ArrowLeft, 
